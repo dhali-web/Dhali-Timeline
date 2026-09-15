6d815d36300df752b99266b6675c20530d903dcd
Ryan M Sullivan <rmmichael95@gmail.com>
2026-09-15 16:50:34 -0700

    Import base controls

--- build/dhali-timeline/index.asset.php
+++ build/dhali-timeline/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '01164dadb3b3740127e3');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '864dcf16bd591fdfc113');
--- src/dhali-timeline/edit.js
+++ src/dhali-timeline/edit.js
@@ -14,6 +14,7 @@ import {
 	useBlockProps,
 	InnerBlocks,
 	InspectorControls,
+	BaseControl,
 	useSettings,
 } from "@wordpress/block-editor";
 /**
