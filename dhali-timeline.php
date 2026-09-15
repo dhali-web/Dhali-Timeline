d9d235d105aaddff5ef3c09e75822a2bba3e67ff
Ryan M Sullivan <rmmichael95@gmail.com>
2026-09-15 15:23:40 -0700

    Changes version num for manager

--- dhali-timeline.php
+++ dhali-timeline.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Dhali Timeline
  * Description:       Example block scaffolded with Create Block tool.
- * Version:           0.1.0
+ * Version:           0.1.1
  * Requires at least: 6.8
  * Requires PHP:      7.4
  * Author:            Dhali Web Development
