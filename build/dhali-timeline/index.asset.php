df1a399e96e28c172030defab56b1afcbed7714b
Ryan M Sullivan <rmmichael95@gmail.com>
2026-09-15 16:55:33 -0700

    Fixed base controls

--- build/dhali-timeline/index.asset.php
+++ build/dhali-timeline/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '864dcf16bd591fdfc113');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e6a1f747a56c3f627542');
