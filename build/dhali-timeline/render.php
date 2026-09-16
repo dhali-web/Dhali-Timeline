098021ec04f8b70bb5dd43fdf59350c899f9126d
Ryan M Sullivan <rmmichael95@gmail.com>
2026-09-16 09:52:40 -0700

    Fixed colors and line size in render.php

--- build/dhali-timeline/render.php
+++ build/dhali-timeline/render.php
@@ -10,9 +10,39 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
+$styles = array();
+
+$line_thickness = isset( $attributes['lineThickness'] )
+	? (int) $attributes['lineThickness']
+	: 8;
+
+$marker_size = isset( $attributes['markerSize'] )
+	? (int) $attributes['markerSize']
+	: 20;
+
+$styles[] = '--dh-timeline-line-width:' . (int) $attributes['lineThickness'] . 'px';
+$styles[] = '--dh-timeline-marker-size:' . (int) $attributes['markerSize'] . 'px';
+
+if ( ! empty( $attributes['activeLineColor'] ) ) {
+	$styles[] = '--dh-timeline-active-color:' . $attributes['activeLineColor'];
+}
+
+if ( ! empty( $attributes['inactiveLineColor'] ) ) {
+	$styles[] = '--dh-timeline-track-color:' . $attributes['inactiveLineColor'];
+}
+
+if ( ! empty( $attributes['activeMarkerColor'] ) ) {
+	$styles[] = '--dh-timeline-marker-active-color:' . $attributes['activeMarkerColor'];
+}
+
+if ( ! empty( $attributes['inactiveMarkerColor'] ) ) {
+	$styles[] = '--dh-timeline-marker-color:' . $attributes['inactiveMarkerColor'];
+}
+
 $wrapper_attributes = get_block_wrapper_attributes(
 	array(
 		'class' => 'dh-timeline',
+		'style' => implode( ';', $styles ),
 	)
 );
 ?>
